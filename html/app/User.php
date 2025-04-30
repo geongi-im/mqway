6c6efd019a49864902c4d8bd580bc8707c9c42d3
imgeongi <imgeongi@gmail.com>
2025-04-30 21:20:10 +0900

    User Auth 추가

--- html/app/User.php
+++ html/app/User.php
@@ -11,12 +11,33 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+     * 테이블 이름 설정
+     *
+     * @var string
+     */
+    protected $table = 'mq_member';
+
+    /**
+     * 기본 키 설정
+     *
+     * @var string
+     */
+    protected $primaryKey = 'idx';
+
+    /**
+     * timestamps 사용 여부
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'mq_user_id', 'mq_password', 'mq_email', 'mq_name', 'mq_level', 'mq_status'
     ];
 
     /**
@@ -25,7 +46,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'mq_password',
     ];
 
     /**
@@ -36,4 +57,65 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * 사용자가 관리자인지 확인
+     *
+     * @return bool
+     */
+    public function isAdmin()
+    {
+        return $this->mq_level === 10;
+    }
+
+    /**
+     * 사용자가 매니저인지 확인
+     *
+     * @return bool
+     */
+    public function isManager()
+    {
+        return $this->mq_level === 9;
+    }
+
+    /**
+     * 이름 접근자
+     *
+     * @return string
+     */
+    public function getNameAttribute()
+    {
+        return $this->mq_name;
+    }
+
+    /**
+     * 이메일 접근자
+     *
+     * @return string
+     */
+    public function getEmailAttribute()
+    {
+        return $this->mq_email;
+    }
+
+    /**
+     * 비밀번호 설정 뮤테이터
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['mq_password'] = $value;
+    }
+
+    /**
+     * 비밀번호 가져오기 접근자
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->mq_password;
+    }
 }
