b9310ab6fb76fc411ded8a1caf4981ff86e0a158
imgeongi <imgeongi@gmail.com>
2025-05-01 23:53:54 +0900

    meta description 추가

--- html/resources/views/layouts/app.blade.php
+++ html/resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="지금 MQWAY에서 당신의 삶을 설계하세요. 경제 지능과 자기주도력을 함께 키우는 통합 교육 플랫폼입니다.">
+
     <meta property="og:image" content="{{ asset('/images/logo/mqway_og_image.png') }}">
     <meta property="og:title" content="{{ config('app.name', 'MQWAY') }}">
     <meta property="og:description" content="경제 지능을 함께 키우는 교육 플랫폼">
