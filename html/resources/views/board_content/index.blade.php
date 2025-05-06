34a02e65330b20cb82ac23925cdc8797b88ea892
imgeongi <imgeongi@gmail.com>
2025-05-06 22:45:05 +0900

    초기화 링크 수정

--- html/resources/views/board_content/index.blade.php
+++ html/resources/views/board_content/index.blade.php
@@ -23,7 +23,7 @@
                         </svg>
                     </button>
                 </div>
-                <a href="{{ route('board-research.index') }}" class="inline-flex items-center justify-center h-12 px-5 bg-white text-cdark rounded-xl hover:bg-gray-50 transition-all">
+                <a href="{{ route('board-content.index') }}" class="inline-flex items-center justify-center h-12 px-5 bg-white text-cdark rounded-xl hover:bg-gray-50 transition-all">
                     <svg class="w-5 h-5 mr-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path>
                     </svg>
