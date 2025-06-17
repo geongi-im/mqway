bb717fc7e66ce76ead37e3403c45d243287a9611
imgeongi <imgeongi@gmail.com>
2025-06-17 11:35:22 +0900

    inverstor_portfolio 테이블에 평균수익률 필드 추가

--- html/app/Models/InvestorPortfolio.php
+++ html/app/Models/InvestorPortfolio.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use App\Models\BoardPortfolio;
 
 class InvestorPortfolio extends Model
 {
@@ -18,11 +19,13 @@ class InvestorPortfolio extends Model
         'portfolio_period',
         'portfolio_value',
         'number_of_stocks',
+        'portfolio_avg_return',
     ];
 
     // 날짜 필드를 Carbon 인스턴스로 변환
     protected $dates = [
-        'record_created_at'
+        'record_created_at',
+        'record_updated_at'
     ];
 
     /**
@@ -31,5 +34,13 @@ class InvestorPortfolio extends Model
     public function details()
     {
         return $this->hasMany(InvestorPortfolioDetail::class, 'p_idx', 'idx');
+    }
+
+    /**
+     * 이 포트폴리오에 연결된 게시판 포트폴리오 정보
+     */
+    public function boardPortfolio()
+    {
+        return $this->hasOne(BoardPortfolio::class, 'mq_portfolio_idx', 'idx');
     }
 } 
