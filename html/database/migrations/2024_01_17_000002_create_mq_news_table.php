0442510ff5f19f3916754af428f316536b2e8c7c
imgeongi <imgeongi@gmail.com>
2026-03-15 21:22:39 +0900

    news 테이블 필드 추가

--- html/database/migrations/2024_01_17_000002_create_mq_news_table.php
+++ html/database/migrations/2024_01_17_000002_create_mq_news_table.php
@@ -18,6 +18,7 @@ class CreateMqNewsTable extends Migration
             $table->text('mq_content')->comment('뉴스 내용');
             $table->string('mq_company', 100)->comment('뉴스 발행사');
             $table->string('mq_source_url')->comment('원본 URL');
+            $table->timestamp('mq_published_date')->nullable()->comment('뉴스 발행일');
             $table->integer('mq_status')->default(1);  // 게시글 상태 (0: 삭제, 1: 활성)
             $table->timestamp('mq_reg_date')->useCurrent();  // 게시글 작성일
             $table->timestamp('mq_update_date')->nullable();  // 게시글 수정일
